8cad56016bd656a99da233aa433dc464a04bf2e0
zihadulislam2 <zihadul708@gmail.com>
2025-11-23 01:04:21 +0600

    fix: cors origin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ✅ Enable CORS
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://yourfrontenddomain.com'], // your frontend URL(s)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if you're using cookies or auth headers
+  });
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
