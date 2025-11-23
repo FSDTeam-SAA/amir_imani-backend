b33c2ba783e8e6a8bfcf1c11cffa02865a34baa1
zihadulislam2 <zihadul708@gmail.com>
2025-11-23 07:04:51 +0600

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // ✅ Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://yourfrontenddomain.com'], // your frontend URL(s)
+    origin: [
+      'http://localhost:3000',
+      'https://yourfrontenddomain.com',
+      'https://amir-imani2025-dashboard.vercel.app',
+    ], // your frontend URL(s)
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // if you're using cookies or auth headers
   });
