8931d524b0676f0da587bc670a2a5b5dca67a964
zihadulislam2 <zihadul708@gmail.com>
2025-11-26 05:41:38 +0600

    gcdgdfgg

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'https://yourfrontenddomain.com',
       'https://amir-imani2025-dashboard.vercel.app',
+      'https://admin.doundogames.com',
     ], // your frontend URL(s)
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // if you're using cookies or auth headers
